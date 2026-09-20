5b42b0cdef326b2a6989b0195f0c48f950e04bb6
Ravi Yadav <raviaur3yadav@gmail.com>
2026-09-20 19:56:34 +0530

    changes code and delete many file

--- server/routes/transcript.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getTranscript } from "../controllers/transcript.controller.js";
-
-const router = express.Router();
-
-router.post("/", getTranscript);
-
-export default router;
